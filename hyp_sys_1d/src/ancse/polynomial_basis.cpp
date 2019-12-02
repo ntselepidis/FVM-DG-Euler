a7aaf6a1ed1b4d8ed495ca88c5995f6a884e2568
Nikolas Tselepidis <nikotselep@hotmail.gr>
2019-12-02 02:18:09 +0100

    Modified Legendre polynomial basis.

--- hyp_sys_1d/src/ancse/polynomial_basis.cpp
+++ hyp_sys_1d/src/ancse/polynomial_basis.cpp
@@ -10,7 +10,7 @@ Eigen::VectorXd PolynomialBasis :: operator() (double xi) const
 	for (int i = 0; i < p+1; i++)
 		poly(i) = std::legendre(i,2*xi-1); // shifted Legendre
 
-    return poly;
+    return scaling_factor*poly;
 }
 
 /// Computes the derivative of Legendre polynomial basis
@@ -23,9 +23,8 @@ Eigen::VectorXd PolynomialBasis :: deriv (double xi) const
 
 	if ( p > 0 ) dpoly(1) = 2.0;
 	if ( p > 1 ) dpoly(2) = 12*xi - 6;
-	if ( p > 2 ) dpoly(3) = 60*xi*xi - 60*xi + 12;
-	if ( p > 3 ) throw std::runtime_error(
-		"Usupported Legendre polynomial order.");
+	if ( p > 2 ) throw std::runtime_error(
+		"Usupported (>2) Legendre polynomial degree.");
 
-    return dpoly;
+    return scaling_factor*dpoly;
 }
--- hyp_sys_1d/tests/test_polynomial_basis.cpp
+++ hyp_sys_1d/tests/test_polynomial_basis.cpp
@@ -6,16 +6,9 @@
 
 TEST(TestLegendre, Example) {
 
-	PolynomialBasis B0(0);
-	PolynomialBasis B1(1);
-	PolynomialBasis B2(2);
-	PolynomialBasis B3(3);
-
-	Eigen::VectorXd L0(1);
-	Eigen::VectorXd L1(2);
-	Eigen::VectorXd L2(3);
-	Eigen::VectorXd L3(4);
-
+	const double scal = 2.0;
+	PolynomialBasis B0(0,scal), B1(1,scal), B2(2,scal);
+	Eigen::VectorXd L0(1), L1(2), L2(3);
 	Eigen::VectorXd xis = Eigen::VectorXd::LinSpaced(10,0.0,1.0);
 
 	for (int j=0; j<xis.cols(); j++) {
@@ -23,27 +16,17 @@ TEST(TestLegendre, Example) {
 		L0 << 1.0;
 		L1 << 1.0, 2*xi-1;
 		L2 << 1.0, 2*xi-1, 6*xi*xi-6*xi+1;
-		L3 << 1.0, 2*xi-1, 6*xi*xi-6*xi+1, 
-			20*xi*xi*xi - 30*xi*xi + 12*xi - 1;
-		ASSERT_EQ( B0(xi), L0 );
-		ASSERT_EQ( B1(xi), L1 );
-		ASSERT_EQ( B2(xi), L2 );
-		ASSERT_EQ( B3(xi), L3 );
+		ASSERT_EQ( B0(xi), scal*L0 );
+		ASSERT_EQ( B1(xi), scal*L1 );
+		ASSERT_EQ( B2(xi), scal*L2 );
 	}
 }
 
 TEST(TestLegendreDeriv, Example) {
 
-	PolynomialBasis B0(0);
-	PolynomialBasis B1(1);
-	PolynomialBasis B2(2);
-	PolynomialBasis B3(3);
-
-	Eigen::VectorXd L0(1);
-	Eigen::VectorXd L1(2);
-	Eigen::VectorXd L2(3);
-	Eigen::VectorXd L3(4);
-
+	const double scal = 2.0;
+	PolynomialBasis B0(0,scal), B1(1,scal), B2(2,scal);
+	Eigen::VectorXd L0(1), L1(2), L2(3);
 	Eigen::VectorXd xis = Eigen::VectorXd::LinSpaced(10,0.0,1.0);
 
 	for (int j=0; j<xis.cols(); j++) {
@@ -51,10 +34,8 @@ TEST(TestLegendreDeriv, Example) {
 		L0 << 0.0;
 		L1 << 0.0, 2.0;
 		L2 << 0.0, 2.0, 12*xi-6;
-		L3 << 0.0, 2.0, 12*xi-6, 60*xi*xi - 60*xi + 12;
-		ASSERT_EQ( B0.deriv(xi), L0 );
-		ASSERT_EQ( B1.deriv(xi), L1 );
-		ASSERT_EQ( B2.deriv(xi), L2 );
-		ASSERT_EQ( B3.deriv(xi), L3 );
+		ASSERT_EQ( B0.deriv(xi), scal*L0 );
+		ASSERT_EQ( B1.deriv(xi), scal*L1 );
+		ASSERT_EQ( B2.deriv(xi), scal*L2 );
 	}
 }
