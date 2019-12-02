627ee6dedf35f8434d4e22ed04b11f21e0cecc99
Nikolas Tselepidis <nikotselep@hotmail.gr>
2019-12-02 01:41:09 +0100

    Implemented Legendre polynomial basis and did minor change to gitignore.

--- hyp_sys_1d/src/ancse/polynomial_basis.cpp
+++ hyp_sys_1d/src/ancse/polynomial_basis.cpp
@@ -5,12 +5,27 @@
 /// at a given reference point xi \in [0,1]
 Eigen::VectorXd PolynomialBasis :: operator() (double xi) const
 {
-    return Eigen::VectorXd::Zero(p+1);
+	Eigen::VectorXd poly(p+1);
+
+	for (int i = 0; i < p+1; i++)
+		poly(i) = std::legendre(i,2*xi-1); // shifted Legendre
+
+    return poly;
 }
 
 /// Computes the derivative of Legendre polynomial basis
 /// at a given reference point xi \in [0,1]
 Eigen::VectorXd PolynomialBasis :: deriv (double xi) const
 {
-    return Eigen::VectorXd::Zero(p+1);
+	Eigen::VectorXd dpoly(p+1);
+
+	dpoly(0) = 0.0;
+
+	if ( p > 0 ) dpoly(1) = 2.0;
+	if ( p > 1 ) dpoly(2) = 12*xi - 6;
+	if ( p > 2 ) dpoly(3) = 60*xi*xi - 60*xi + 12;
+	if ( p > 3 ) throw std::runtime_error(
+		"Usupported Legendre polynomial order.");
+
+    return dpoly;
 }
