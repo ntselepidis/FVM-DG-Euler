463160548b5e8a9d1e4d91107f627920cedef0fa
Nikolas Tselepidis <nikotselep@hotmail.gr>
2019-12-03 23:20:10 +0100

    Implemented computation of DG volume integral.[C

--- hyp_sys_1d/src/ancse/dg_rate_of_change.cpp
+++ hyp_sys_1d/src/ancse/dg_rate_of_change.cpp
@@ -23,7 +23,49 @@ void DGRateOfChange<NumericalFlux>
 :: eval_volume_integral(Eigen::MatrixXd &dudt,
                         const Eigen::MatrixXd &u0) const
 {
-    // implement the loop for DG volume integral.
+    // Implements the loop for DG volume integral.
+    // Input:
+    //   - u0: ( m x (p+1) ) x N
+    // Output:
+    //   - dudt ( updated ): ( m x (p+1) ) x N
+    // Note:
+    //   - ( m x (p+1) ) x N => ( n_vars x n_coeff ) x n_cells
+
+    // Helpers
+    const int n_coeff = 1 + poly_basis.get_degree();
+    const int n_quad_points = static_cast<int>(quad_points.size());
+    const int n_cells = grid.n_cells;
+    const int n_ghost = grid.n_ghost;
+    const int n_vars = model->get_nvars();
+
+    // Eval basis derivate wrt x (dphi) for all quad points
+    Eigen::MatrixXd dphi(n_coeff, n_quad_points);
+    for (int k = 0; k < n_quad_points; k++)
+    {
+        dphi.col(k) = poly_basis.deriv( quad_points(k) );
+    }
+
+    // Loop over all cells (except for ghosts)
+    for (int j = n_ghost-1; j < n_cells-n_ghost; j++)
+    {
+        // Loop over all quadrature points
+        for (int k = 0; k < n_quad_points; k++)
+        {
+            // Build sol u on quad point k ( n_vars-vector )
+            Eigen::VectorXd u_k = dg_handler.build_sol(
+                         u0.col(j), quad_points(k) );
+
+            // Compute flux on quad point k ( n_vars-vector )
+            Eigen::VectorXd f_k = model->flux( u_k );
+
+            // Loop over all components of flux vector
+            for (int i = 0; i < n_vars; i++)
+            {
+                dudt.col(j).segment(i*n_coeff, n_coeff)
+                        += quad_weights(k)*f_k(i)*dphi.col(k);
+            }
+        }
+    }
 }
 
 
