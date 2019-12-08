e86264a95374f6144402b3a98183f73c5be5e8c6
Nikolas Tselepidis <nikotselep@hotmail.gr>
2019-12-08 13:43:00 +0100

    Implemented minmod, superbee, and mc limiters.

--- unstructured_euler/slope_limiter.hpp
+++ unstructured_euler/slope_limiter.hpp
@@ -6,8 +6,21 @@ double minmod(double a, double b) {
     return 0.5 * (sign(a) + sign(b)) * std::min(std::abs(a), std::abs(b));
 }
 
+double maxmod(double a, double b) {
+    return 0.5 * (sign(a) + sign(b)) * std::max(std::abs(a), std::abs(b));
+}
+
+double minmod(double a, double b, double c) {
+    return minmod(a, minmod(b, c));
+}
+
+// Uses minmod unless having explicitly defined superbee or mc
 double slope_limiter(double sL, double sR) {
-    // Implement some slope limiter you know for scalar
-    // conservation laws in 1D.
-    return 0.0;
+#if defined(ANCSE_SUPERBEE_LIMITER)
+	return maxmod( minmod(2.0 * sL, sR), minmod(sL, 2.0 * sR) );
+#elif defined(ANCSE_MC_LIMITER)
+	return minmod( 2.0 * sL, 0.5 * (sL + sR), 2.0 * sR );
+#else
+	return minmod( sL, sR );
+#endif
 }
