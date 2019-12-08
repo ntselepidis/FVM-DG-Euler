994707000db9ebbd0b99998709468c4ceb38dac8
Nikolas Tselepidis <nikotselep@hotmail.gr>
2019-12-08 14:17:32 +0100

    Implemented CFL condition for unstructured grid.

--- unstructured_euler/cfl_condition.hpp
+++ unstructured_euler/cfl_condition.hpp
@@ -10,10 +10,17 @@ class CFLCondition {
     explicit CFLCondition(const Mesh &mesh) : dx(mesh.getMinimumInradius()) {}
 
     double operator()(const Eigen::MatrixXd &U) const {
-        // compute the cfl condition here.
-        // you can use `assert_valid_timestep` to check if
-        // the computed value is valid.
-        return 0.001;
+
+        double a_max = 0.0;
+        for (int i = 0; i < U.rows(); i++) {
+            a_max = std::max( a_max, euler::maxEigenValue( U.row(i) ) ); // TODO: Check implicit conversion from row vector to col vector (i.e. EulerState)
+        }
+
+        double dt = cfl_number * dx / a_max;
+
+        assert_valid_timestep( dt );
+
+        return dt;
     }
 
     void assert_valid_timestep(double dt_cfl) const {
